239a6ea77be657a18795c2b10a532feb32ba28fb
Jack Curtin <jack23curtin@gmail.com>
2018-02-10 22:25:42 -0500

    Update knight.py

--- knight.py
+++ knight.py
@@ -1,38 +1,40 @@
 class Knight:
-    def __init__(self, x, y, team, ID):
+
+    def __init__(self,x,y,team,allPiece):
         self.x = x
         self.y = y
         self.team = team
-        self.ID = ID
-        self.imagefile = "assets/" + team + "knight.png"
-        self.image = pygame.image.load(self.imagefile)
-        self.rect = self.image.get_rect()
-        self.rect.center = self.x, self.y
+        self.allPiece = allPiece
 
-    def validMoves(self, list_of_pieces):
+    def validMoves(self,allPiece):
         """
         Checks vaild moves for knight
         Inputs: allPiece
         Outputs: Array of arrays of possible positions (possibleMoves)
         """
-        movelist = []
-        listofnew = [(self.x + 2, self.y + 1), (self.x + 2, self.y - 1), (self.x - 2, self.y + 1),
-                     (self.x - 2, self.y - 1), (self.x + 1, self.y + 2), (self.x + 1, self.y - 2),
-                     (self.x - 1, self.y + 2), (self.x - 1, self.y - 2)]
-        for j in listofnew:
-            for i in list_of_pieces:
-                if j[0] == i.x and j[1] == i.y and self.team == i.team:
-                # do nothing
-                else:
-                    if 0 < j[0] < 9 and 0 < j[1] < 9:
-                        movelist.append(j[0], j[1])
-        return movelist
+        postion = (self.x, self.y)
+        allPiece = "things"
+        moveList = []
+        moveList.append([self.x + 2, self.y + 1])
+        moveList.append([self.x - 2, self.y + 1])
+        moveList.append([self.x + 2, self.y - 1])
+        moveList.append([self.x - 2, self.y - 1])
 
-    def movePiece(self, move):
+        moveList.append([self.x + 1, self.y + 2])
+        moveList.append([self.x - 1, self.y + 2])
+        moveList.append([self.x + 1, self.y - 2])
+        moveList.append([self.x - 1, self.y - 2])
+        for i in range(len(moveList)):
+            if team is in allPiece[i][3]:
+                del allPiece[i]
+        for i in range(len(moveList)):
+            #checks if piece color is same as possible move place (CAN'T MOVE BLACK ON BLACK) gets rid of same color squares
+            if team is in allPiece[i]["BLACK OR WHITE"]:
+                del moveList[i]
+
+    def movePiece(possibleMoves):
         """
         MovesPiece to vaid space
         Inputs: possibleMoves
         Outputs: New pawn position
         """
-        self.x = move[0]
-        self.y = move[1]
