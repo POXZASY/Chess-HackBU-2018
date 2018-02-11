fd0bb54cfd0a0ec2cb5912a70609f3e3a9935254
Unknown <lwisdom1@binghamton.edu>
2018-02-10 21:20:44 -0500

    Auto stash before merge of "master" and "origin/master"

--- capture.py
+++ capture.py
@@ -1,7 +1,7 @@
 import pygame
 import Chessboard
 
-def capture(piece,list_of_pieces):
-    for i in list_of_pieces:
-        if i.x == piece.x and i.y == piece.y:
-            #Delete i forever
+def capture(piece,list_of_pieces,chessboard):
+    for i in chessboard.dict_of_pieces:
+        if chessboard.dict_of_pieces[i].x == piece.x and chessboard.dict_of_pieces[i].y == piece.y:
+            chessboard.dict_of_pieces.pop(i)
